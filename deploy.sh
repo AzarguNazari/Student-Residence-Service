07d28b30eed9522adc45b2e747b90ab876c70a48
Hazar Gul Nazari <hazar.nazari@favendo.com>
2021-03-27 19:28:33 +0100

    updates

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@
 ./mvnw spring-boot:build-image -pl backend/Bulletinboard-Service/pom.xml
 ./mvnw spring-boot:build-image -pl backend/Spring-Boot-Admin/pom.xml
 
+docker-compose -f config/docker-compose.yml up -d
 docker-compose -f backend/docker-compose-application.yml up -d
 docker-compose -f frontend/docker-compose.yml up -d
